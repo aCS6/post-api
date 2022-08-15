2de2d778ece3389e71c542d1e5d8f208f06f354a
amir <amir.cse006@gmail.com>
2022-08-15 22:41:00 +0600

    added schema and documentation

--- src/settings.py
+++ src/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "allauth.socialaccount",
     "dj_rest_auth",
     "dj_rest_auth.registration",
+    "drf_spectacular",
 
     "accounts",
     "posts",
@@ -60,6 +61,14 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ],
+
+    "DEFAULT_SCHEMA_CLASS" : "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS= {
+    "TITLE" : "Post Api",
+    "DESCRIPTION" : "A simple API",
+    "VERSION" : "0.0.1"
 }
 
 MIDDLEWARE = [
--- src/urls.py
+++ src/urls.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -8,4 +13,7 @@ urlpatterns = [
     path("api/v1/dj-rest-auth/", include("dj_rest_auth.urls")),
     path("api/v1/dj-rest-auth/registration/",
             include("dj_rest_auth.registration.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"), # auto generate schema
+    path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 ]
